3e301fb34fed5a81c5c065b2a63f98bcc825a5b4
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-08-03 09:50:09 +0530

    update

---
+++ src/providers/Providers.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { lightTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { createConfig } from "wagmi";
+import { http } from "viem";
+import { somniaTestnet } from "viem/chains"
+import { createStorage } from 'wagmi';
+import { farcasterFrame } from '@farcaster/frame-wagmi-connector';
+import DataContextProvider from "@/context/DataContext";
+
+import { sdk } from "@farcaster/frame-sdk";
+import { useEffect } from "react";
+
+const config = createConfig({
+  chains: [somniaTestnet],
+  transports: {
+    [somniaTestnet.id]: http(),
+  },
+  storage: createStorage({ storage: typeof window !== 'undefined' ? window.localStorage : undefined }),
+  connectors: [
+    farcasterFrame()
+  ]
+});
+
+const queryClient = new QueryClient();
+
+const Providers = ({ children }) => {
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await sdk.actions.ready();
+      } catch (error) {
+        console.error("Failed to initialize Farcaster:", error);
+      }
+    };
+    init();
+  }, []);
+  return (
+    <>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider
+            theme={lightTheme({
+              accentColor: "#6c54f8",
+              accentColorForeground: "white",
+              borderRadius: "medium",
+            })}
+            modalSize="wide"
+          >
+            <DataContextProvider>{children}</DataContextProvider>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
+  );
+};
+
+export default Providers;
